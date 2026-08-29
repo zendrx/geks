03d2611bf357bbf7b0aba621ea0be5012e6fad43
ZenDrx <ynwghosted@icloud.com>
2026-08-29 07:03:04 +0000

    Save auth.cr.

--- src/routes/auth.cr
+++ src/routes/auth.cr
@@ -1,7 +1,7 @@
 require "kemal"
 require "../auth/*"
 
-post "/signup" do |env|
+post "/api/signup" do |env|
  email = env.params.json["email"].as(String)
  psswd = env.params.json["password"].as(String)
  username = env.params.json["username"].as(String)
@@ -9,14 +9,14 @@ post "/signup" do |env|
  signup.to_json
  end
  
- post "/login" do |env|
+ post "/api/login" do |env|
      uname = env.params.json["username"].as(String)
      psswd = env.params.json["password"].as(String)
      login = Geks::Auth.login(usernam, psswd)
      login.to_json
  end
  
- post "/forget" do |env|
+ post "/api/forget" do |env|
      username = env.params.json["username"].as(String)
      email = env.params.json["email"].as(String)
      new_pswd = env.params.json["new_pswd"].as(String)
@@ -24,7 +24,7 @@ post "/signup" do |env|
      fg.to_json
  end
  
- post "/me" do |env|
+ post "/api/me" do |env|
      id = env.request.headers["Authorization"]?
      if id && id.starts_with?("Bearer ")
          user_id = id[7..-1]
