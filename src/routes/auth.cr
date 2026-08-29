682b59e5fb4c7bb2f8ead07ea2fb34cd5f59d7d7
ZenDrx <ynwghosted@icloud.com>
2026-08-29 06:22:00 +0000

    Save auth.cr.

---
+++ src/routes/auth.cr
@@ -0,0 +1,34 @@
+require "kemal"
+require "../auth/*"
+
+post "/signup" do |env|
+ email = env.params.json["email"].as(String)
+ psswd = env.params.json["password"].as(String)
+ username = env.params.json["username"].as(String)
+ signup = Geks::Auth.signup(email, psswd, username)
+ signup.to_json
+ end
+ 
+ post "/login" do |env|
+     uname = env.params.json["username"].as(String)
+     psswd = env.params.json["password"].as(String)
+     login = Geks::Auth.login(usernam, psswd)
+     login.to_json
+ end
+ 
+ post "/forget" do |env|
+     username = env.params.json["username"].as(String)
+     email = env.params.json["email"].as(String)
+     new_pswd = env.params.json["new_pswd"].as(String)
+     fg = Geks::Auth.forget(email, username)
+     fg.to_json
+ end
+ 
+ post "/me" do |env|
+     id = env.request.headers["Authorization"]?
+     if id && id.starts_with?("Bearer ")
+         user_id = id[7..-1]
+     db = Geks::Db::User.get_user_data(user_id)
+     db.to_json
+ end
+ 
